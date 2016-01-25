d83d3ecf4e62ce9881b54639855568ba175326de
Wouter <info@wouterkelderman.nl>
2016-01-25 23:00:22 +0100

    AH scraper
    local development of the AH scraper
    *date for action period
    *create file for database migration

--- app/Feeds/AH/Factory.php
+++ app/Feeds/AH/Factory.php
@@ -5,34 +5,50 @@ namespace App\Feeds\AH;
 use Carbon\Carbon;
 use DOMDocument;
 use DOMXPath;
+use App\Functions;
 
 class Factory
 {
     public function process($dataset)
     {
     	foreach ($dataset as $data) {
+	    	Carbon::setWeekStartsAt(Carbon::MONDAY);
+			Carbon::setWeekEndsAt(Carbon::SUNDAY);
+    		$functions = new Functions;
 	    	$raw = json_decode($data, true);
 	    	$lanes = $raw['_embedded']['lanes'];
-
 	    	foreach ($lanes as $lane) {
 	    		$categories = $lane['_embedded']['items'];
 	    		foreach ($categories as $categorie) {
 	    			if ($categorie['resourceType'] == 'Product') {
 	    				$productLabel = $categorie['_embedded']['productCard'];
-	    				//var_dump($productLabel);
+	    				var_dump($productLabel);
 	    				$product = [];
+	    				$product['week'] = $functions->findWeek($productLabel['navItem']['link']['href']);
+	    				$product['actionDateStart'] = Carbon::now()->startOfWeek()->toDateString();
+	    				$product['actionDateEnd'] = Carbon::now()->endOfWeek()->toDateString();
+	    				$product['orginalPrice'] = null;
 	    				$product['categorie'] = $productLabel['label'];
 	    				$product['name'] = $productLabel['navItem']['title'];
 	    				$productCard = $productLabel['_embedded']['product'];
 	    				//var_dump($productCard);
 	    				$product['unitSize'] = $productCard['unitSize'];
-	    				$product['price']['orginalPrice'] = $productCard['priceLabel']['was'];
-	    				$product['price']['actionPrice'] = $productCard['priceLabel']['now'];
-	    				$product['period'] = $productCard['discount']['period'];
+	    				if ( array_key_exists('was', $productCard['priceLabel']) ) {
+	    					$product['orginalPrice'] = $productCard['priceLabel']['was'];
+	    				}
+	    				$product['actionPrice'] = $productCard['priceLabel']['now'];
+	    				if ( array_key_exists('discount', $productCard) ) {
+		    				var_dump(array_key_exists('period', $productCard['discount']));
+		    				if ( array_key_exists('period', $productCard['discount']) ) {
+		    					$product['period'] = $productCard['discount']['period'];
+		    					$product['label'] = $productCard['discount']['label'];
+		    				}
+		    			}
 	    				$product['AHid'] = $productCard['id'];
+	    				
 	    				//dd($categorie);
 	    				var_dump($product);
-	    				dd('doei');
+	    				//dd('doei');
 	    			}
 	    		}
 	    	}
---
+++ app/Functions.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use DOMDocument;
+use DOMXPath;
+
+class Functions
+{
+	public function findWeek($string) {
+		$result = $this->between('week/', '?group', $string);
+		return $result;
+	}
+
+	public function before($first, $inthat)
+	{
+   		return substr($inthat, 0, strpos($inthat, $first));
+	}
+	
+	public function before_last ($first, $inthat)
+	{
+		return substr($inthat, 0, $this->strrevpos($inthat, $first));
+	}
+
+	public function after($first, $inthat)
+	{
+		if (!is_bool(strpos($inthat, $first))) {
+			return substr($inthat, strpos($inthat,$first)+strlen($first));
+		}
+	}
+
+	public function after_last ($first, $inthat)
+	{
+		if (!is_bool($this->strrevpos($inthat, $first))) {
+			return substr($inthat, $this->strrevpos($inthat, $first)+strlen($first));
+		}
+	}
+
+	public function between($first, $that, $inthat)
+	{
+		return $this->before ($that, $this->after($first, $inthat));
+	}
+
+	public function between_last ($first, $that, $inthat)
+	{
+		return $this->after_last($first, $this->before_last($that, $inthat));
+	}
+
+	public function strrevpos($instr, $needle)
+	{
+		$rev_pos = strpos (strrev($instr), strrev($needle));
+		if ($rev_pos===false) {
+			return false;
+		} else {
+			return strlen($instr) - $rev_pos - strlen($needle);
+		}
+	}
+}
